c1d8cebd5d0fecb23d76c0d08c6f80fa7076b380
Karen Mkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2026-03-12 18:04:57 -0700

    test adc

--- board_test.py
+++ board_test.py
@@ -1,6 +1,7 @@
 import time
 import board
 import digitalio
+import mc3008
 
 PIN = board.D4
 
@@ -8,9 +9,33 @@ print("hello blinky!")
 
 led = digitalio.DigitalInOut(PIN)
 led.direction = digitalio.Direction.OUTPUT
+adc = mcp3008.MCP3008()
 
 while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
+    # LED test
+    for i in range(5):
+        led.value = True
+        time.sleep(0.5)
+        led.value = False
+        time.sleep(0.5)
+
+    # Light sensor test
+    for i in range(50):
+        print(adc.read([mcp3008.CH0]))
+        time.sleep(.1)
+
+    for i in range(4):
+        led.value = True
+        time.sleep(0.2)
+        led.value = False
+        time.sleep(0.2)
+    # Sound sensor test
+    for i in range(50):
+        print(adc.read([mcp3008.CH1]))
+        time.sleep(.1)
+
+    for i in range(4):
+        led.value = True
+        time.sleep(0.1)
+        led.value = False
+        time.sleep(0.1)
